6a1916fb6809954502f0cd4cc5e2412bba0dfd7d
Ian Darwin <ian@darwinsys.com>
2015-11-02 18:45:34 -0800

    This code should get as far as sending a Report.

--- javaclient/src/main/java/crashburnfree/client/CrashBurnFree.java
+++ javaclient/src/main/java/crashburnfree/client/CrashBurnFree.java
@@ -1,7 +1,13 @@
 package crashburnfree.client;
 
-import java.awt.PageAttributes.MediaType;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Base64;
+import java.util.Date;
+import java.util.regex.Pattern;
 
 public class CrashBurnFree {
 
@@ -21,20 +27,25 @@ public class CrashBurnFree {
 	public void establish() {
 		Thread.setDefaultUncaughtExceptionHandler(
 				new Thread.UncaughtExceptionHandler(){
-					public void uncaughtException(Thread t, Throwable ex){
+					public void uncaughtException(Thread t, Throwable ex) {
 						System.out.println(
 								"CrashBurnFree.establish(): Crashed in thread " + t.getName());
 						System.out.println(
 								"CrashBurnFree.establish(): Handler running in thread " + t.getName());
 						System.out.println(
 								"CrashBurnFree.establish():Exception was: " + ex.toString());
-//						Acknowledgement ack = ClientBuilder.newClient()
-//								.target(URL)
-//								.request(MediaType.APPLICATION_JSON)
-//								.header(AUTH_HEADER, encodeAuth("robin", "secret54321"))
-//								.get(Acknowledgement.class);
-//						System.out.println("Got an event: " + ack);
-						
+						Report r = new Report();
+						r.opSys = System.getProperty("system.os.name");
+						r.osVer = System.getProperty("system.os.version");
+						r.when = new Date();
+						r.description = "Happened on thread " + t.getName();
+						r.exception = ex;
+						try {
+							send(r);
+						} catch (Exception e) {
+							System.err.println("Failed to send exception to reporter: " + ex);
+							// The end of the line - we give up
+						}
 					}
 				});
 
@@ -46,9 +57,27 @@ public class CrashBurnFree {
 	 * @param t The Throwable
 	 * @return The Throwable in JSON format
 	 */
-	public String exceptionToJSON(Throwable t) {
-		//return "{\"cause\":{\"cause\":null,\"stackTrace\":[{\"methodName\":\"main\",\"fileName\":\"PlayHouse.java\",\"lineNumber\":10,\"className\":\"play.PlayHouse\",\"nativeMethod\":false}],\"message\":\"Testing 1 2 3\",\"localizedMessage\":\"Testing 1 2 3\",\"suppressed\":[]},\"stackTrace\":[{\"methodName\":\"main\",\"fileName\":\"PlayHouse.java\",\"lineNumber\":11,\"className\":\"play.PlayHouse\",\"nativeMethod\":false}],\"message\":\"How's this?\",\"localizedMessage\":\"How's this?\",\"suppressed\":[]}";
-		return "{\"class\":\"" + t.getClass().getName() + "\",\"message\":\"" + t.getMessage() + "\",\"localizedMessage\":\"How's this?\",\"suppressed\":[]}";
+	public int send(Report r) throws Exception {
+		URL url = new URL(URL);
+		URLConnection conn = url.openConnection();
+		conn.setDoInput(true);
+		conn.setDoOutput(true);
+		conn.setAllowUserInteraction(true);
 
+		conn.connect();
+
+		PrintWriter out = new PrintWriter(conn.getOutputStream());
+		out.println(JsonConverters.ReportToJSON(r));
+		out.close();			// Important!
+
+		BufferedReader in = new BufferedReader(
+				new InputStreamReader(conn.getInputStream()));
+		String line = in.readLine(); // Must be of form HTTP/1.1 200 OK
+		in.close();
+		Pattern regex = Pattern.compile("HTTP/\\d\\.\\d\\s+(\\d{3})\\s+.*");
+		if (!regex.matcher(line).matches()) {
+			throw new IllegalStateException("Unknown response from web service");
+		}
+		return Integer.parseInt(regex.matcher(line).group(1));
 	}
 }
