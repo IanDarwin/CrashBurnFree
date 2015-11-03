cc26a6bbbf703ab2c001fbc2eceafb66e56d62d1
Ian Darwin <ian@darwinsys.com>
2015-11-02 23:03:57 -0800

    Correct argument list

--- javaserver/src/main/java/crashburnfree/javaserver/JavaServer.java
+++ javaserver/src/main/java/crashburnfree/javaserver/JavaServer.java
@@ -1,14 +1,12 @@
 package crashburnfree.javaserver;
 
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.PersistenceUnit;
-import javax.transaction.Transactional;
-import javax.transaction.Transactional.TxType;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
+
+import crashburnfree.client.Report;
 
 @Path("/submit")
 public class JavaServer {
@@ -18,8 +16,8 @@ public class JavaServer {
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	// @Transactional(value=TxType.REQUIRED)
-	public void upload(Throwable t, @HeaderParam("Authorization") String authorization) {
-		System.out.println("JavaServer.upload: " + t);
+	public void upload(Report r, @HeaderParam("Authorization") String authorization) {
+		System.out.println("JavaServer.upload: " + r);
 		
 		// Here you'd have to actually do something with the Throwable.
 		
